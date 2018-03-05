ba4b50b5a9906551ba95b20cc3e91b713e177a03
chengzhiyu <834882283@qq.com>
2018-03-05 14:33:43 +0800

    update map example

---
+++ practices/map.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"sync"
+	"fmt"
+	"time"
+)
+
+func main() {
+	channelTest()
+	var m sync.Map
+
+	for i := 0; i < 3; i++ {
+		go func(i int) {
+			for j := 0; ; j++ {
+				m.Store(i, j)
+			}
+		}(i)
+	}
+
+	for i := 0; i < 10; i++ {
+		m.Range(func(key, value interface{}) bool {
+			fmt.Printf("%d: %d\t", key, value)
+			return true
+		})
+		fmt.Println()
+		time.Sleep(time.Second)
+	}
+}
+
+func channelTest() {
+	messages := make(chan string)
+	go func() {
+		messages <- "10"
+	}()
+
+	fmt.Println(<-messages)
+}
