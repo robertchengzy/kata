10bf0999e9c8289ea5f73ecd7dee52551525a0a5
chengzhiyu <rsdczhs0325>
2018-04-09 09:29:39 +0800

    go tool update

--- utils/tool.go
+++ utils/tool.go
@@ -5,6 +5,7 @@ import (
 	"reflect"
 	"fmt"
 	"errors"
+	"math"
 )
 
 // 文件名过滤器
@@ -49,6 +50,59 @@ func (s *MyStruct) FillStruct(m map[string]interface{}) error {
 		}
 	}
 	return nil
+}
+
+const MIN = 0.000001
+// MIN 为用户自定义的比较精度
+func IsEqual(f1, f2 float64) bool {
+	return math.Dim(f1, f2) < MIN
+}
+
+//截取字符串 start 起点下标 length 需要截取的长度
+func Substr(str string, start int, length int) string {
+	rs := []rune(str)
+	rl := len(rs)
+	end := 0
+
+	if start < 0 {
+		start = rl - 1 + start
+	}
+	end = start + length
+
+	if start > end {
+		start, end = end, start
+	}
+
+	if start < 0 {
+		start = 0
+	}
+	if start > rl {
+		start = rl
+	}
+	if end < 0 {
+		end = 0
+	}
+	if end > rl {
+		end = rl
+	}
+
+	return string(rs[start:end])
+}
+
+//截取字符串 start 起点下标 end 终点下标(不包括)
+func Substr2(str string, start int, end int) string {
+	rs := []rune(str)
+	length := len(rs)
+
+	if start < 0 || start > length {
+		panic("start is wrong")
+	}
+
+	if end < 0 || end > length {
+		panic("end is wrong")
+	}
+
+	return string(rs[start:end])
 }
 
 func main() {
