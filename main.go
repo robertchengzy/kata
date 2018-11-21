9f9d5c892b01417ff55a5aba9b8dbfcac0111cf7
ChengZhiYu <chengzy@ecqun.com>
2018-11-21 15:19:14 +0800

    go 阻塞进程

--- main.go
+++ main.go
@@ -2,8 +2,15 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
 func main() {
 	fmt.Println("good good study, day day up")
+
+	sig := make(chan os.Signal, 2)
+	signal.Notify(sig, syscall.SIGTERM, syscall.SIGINT)
+	<-sig
 }
