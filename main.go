d953ffa9e7f254a80f945e5cf4ff582e15b19f02
ChengZhiYu <chengzy@ecqun.com>
2019-04-12 16:56:58 +0800

    study

--- main.go
+++ main.go
@@ -2,8 +2,23 @@ package main
 
 import (
 	"fmt"
+	"hash/crc32"
 )
 
 func main() {
 	fmt.Println("good good study, day day up")
+	fmt.Println(HashCode("1234567889"))
+}
+
+// 获取hashcode
+func HashCode(s string) int {
+	v := int(crc32.ChecksumIEEE([]byte(s)))
+	if v >= 0 {
+		return v
+	}
+	if -v >= 0 {
+		return -v
+	}
+	// v == MinInt
+	return 0
 }
