a3716eda2a711031eb13317b4815fad81cd004db
程智宇 <chengzy@ecqun.com>
2019-12-04 09:08:45 +0800

    study

--- foundatimentals/top-interview-question/basics/string_array.go
+++ foundatimentals/top-interview-question/basics/string_array.go
@@ -124,7 +124,7 @@ func DominantIndex1(nums []int) int {
 }
 
 /*
-	对角线遍历
+对角线遍历
 给定一个含有 M x N 个元素的矩阵（M 行，N 列），请以对角线遍历的顺序返回这个矩阵中的所有元素，对角线遍历如下图所示。
 示例:
 输入:
@@ -135,7 +135,6 @@ func DominantIndex1(nums []int) int {
 ]
 输出:  [1,2,4,7,5,3,6,8,9]
 */
-
 func FindDiagonalOrder(matrix [][]int) []int {
 	m := len(matrix)
 	if m == 0 {
@@ -171,4 +170,31 @@ func FindDiagonalOrder(matrix [][]int) []int {
 	}
 
 	return arr
+}
+
+/*
+螺旋矩阵
+给定一个包含 m x n 个元素的矩阵（m 行, n 列），请按照顺时针螺旋顺序，返回矩阵中的所有元素。
+
+示例 1:
+
+输入:
+[
+ [ 1, 2, 3 ],
+ [ 4, 5, 6 ],
+ [ 7, 8, 9 ]
+]
+输出: [1,2,3,6,9,8,7,4,5]
+示例 2:
+
+输入:
+[
+  [1, 2, 3, 4],
+  [5, 6, 7, 8],
+  [9,10,11,12]
+]
+输出: [1,2,3,4,8,12,11,10,9,5,6,7]
+*/
+func spiralOrder(matrix [][]int) []int {
+
 }
