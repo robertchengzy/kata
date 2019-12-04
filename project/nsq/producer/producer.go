8455ff29023fea4258ef1544e73b149ad55bca82
程智宇 <chengzy@ecqun.com>
2019-12-04 09:12:14 +0800

    study

--- project/nsq/producer/producer.go
+++ project/nsq/producer/producer.go
@@ -1,15 +1,24 @@
 package main
 
 import (
+	"fmt"
 	"github.com/nsqio/go-nsq"
 	"log"
-	"strconv"
-	"time"
+	"os"
 )
 
 var producer *nsq.Producer
 
 func main() {
+	if len(os.Args) < 3 {
+		fmt.Println("参数不足")
+		return
+	}
+	env := os.Args[1]
+	param := os.Args[2]
+
+	fmt.Printf("data [%v] [%v]", env, param)
+
 	nsqd := "192.168.1.51:4150"
 	var err error
 	producer, err = nsq.NewProducer(nsqd, nsq.NewConfig())
@@ -23,13 +32,13 @@ func main() {
 		panic(err)
 	}
 
-	for i := 0; i < 100; i++ {
-		err = producer.Publish("test", []byte("nihao"+strconv.Itoa(i)))
+	for i := 0; i < 1; i++ {
+		data := `{"flexibleTaskRenew":` + param + `}`
+		err = producer.Publish("j_pp_phoneBill", []byte(data))
 		if err != nil {
 			log.Fatalln(err)
 		}
 
 		log.Println("send success", i)
-		time.Sleep(3 * time.Second)
 	}
 }
