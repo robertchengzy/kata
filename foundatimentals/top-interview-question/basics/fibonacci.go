30db6c6baeb5cd460b7fd38c146d6a2f1f8a9a23
程智宇 <chengzy@ecqun.com>
2020-05-12 17:44:52 +0800

    study

--- foundatimentals/top-interview-question/basics/fibonacci.go
+++ foundatimentals/top-interview-question/basics/fibonacci.go
@@ -36,3 +36,16 @@ func fibonaccifuncTest() {
 		fmt.Println(f())
 	}
 }
+
+func fibonacciSelect(c, quit chan int) {
+	x, y := 0, 1
+	for {
+		select {
+		case c <- x:
+			x, y = y, x+y
+		case <-quit:
+			fmt.Println("quit")
+			return
+		}
+	}
+}
