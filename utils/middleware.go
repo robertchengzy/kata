9b087f285b537f121bccbe1803fbd56f4f44fed0
程智宇 <chengzy@ecqun.com>
2020-06-11 15:54:29 +0800

    study http

--- utils/middleware.go
+++ utils/middleware.go
@@ -1,6 +1,11 @@
 package utils
 
-import "net/http"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+)
 
 type Middleware func(handlerFunc http.HandlerFunc) http.HandlerFunc
 
@@ -24,4 +29,45 @@ func createNewMiddleware() Middleware {
 
 	// Return newly created middleware
 	return middleware
+}
+
+func test() {
+	http.HandleFunc("/", chain(Hello, Logging(), Method("GET")))
+	http.ListenAndServe(":8080", nil)
+}
+
+func Logging() Middleware {
+	return func(handlerFunc http.HandlerFunc) http.HandlerFunc {
+		return func(w http.ResponseWriter, r *http.Request) {
+			start := time.Now()
+			defer func() { log.Println(r.URL.Path, time.Since(start)) }()
+
+			handlerFunc(w, r)
+		}
+	}
+}
+
+func Method(m string) Middleware {
+	return func(handlerFunc http.HandlerFunc) http.HandlerFunc {
+		return func(w http.ResponseWriter, r *http.Request) {
+			if r.Method != m {
+				http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+				return
+			}
+
+			handlerFunc(w, r)
+		}
+	}
+}
+
+func chain(f http.HandlerFunc, middlewares ...Middleware) http.HandlerFunc {
+	for _, m := range middlewares {
+		f = m(f)
+	}
+
+	return f
+}
+
+func Hello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "hello world")
 }
