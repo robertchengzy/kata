0be10641c01346ad753aff3ea268f77e447cd465
程智宇 <chengzy@ecqun.com>
2020-06-11 15:43:27 +0800

    study http

---
+++ utils/middleware.go
@@ -0,0 +1,27 @@
+package utils
+
+import "net/http"
+
+type Middleware func(handlerFunc http.HandlerFunc) http.HandlerFunc
+
+func createNewMiddleware() Middleware {
+
+	// Create a new Middleware
+	middleware := func(next http.HandlerFunc) http.HandlerFunc {
+
+		// Define the http.HandlerFunc which is called by the server eventually
+		handler := func(w http.ResponseWriter, r *http.Request) {
+
+			// ... do middleware things
+
+			// Call the next middleware/handler in chain
+			next(w, r)
+		}
+
+		// Return newly created handler
+		return handler
+	}
+
+	// Return newly created middleware
+	return middleware
+}
