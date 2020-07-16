20847e4adc5f80de138a6014cc0785b4c1dadc2f
程智宇 <chengzy@ecqun.com>
2020-07-16 14:31:46 +0800

    study

--- main.go
+++ main.go
@@ -2,19 +2,8 @@ package main
 
 import (
 	"fmt"
-	"kata/foundatimentals/structure/queue"
-	"math"
 )
 
 func main() {
 	fmt.Println("good good study, day day up")
-
-	fmt.Println([]rune("0123456789"))
-
-	fmt.Println((48-'0'+1)%10 + '0')
-	fmt.Println((57-'0'+1)%10 + '0')
-
-	fmt.Println(math.Sqrt(float64(12)))
-
-	fmt.Println(queue.NumSquares(4))
 }
