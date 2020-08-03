edd404609ffcb6c5f4f37e5976b09e2467b6a34a
程智宇 <chengzy@ecqun.com>
2020-08-03 17:07:19 +0800

    study

---
+++ foundatimentals/structure/array_string/string/longest_common_prefix.go
@@ -0,0 +1,39 @@
+package string
+
+/*
+最长公共前缀
+编写一个函数来查找字符串数组中的最长公共前缀。
+如果不存在公共前缀，返回空字符串 ""。
+示例 1:
+输入: ["flower","flow","flight"]
+输出: "fl"
+示例 2:
+输入: ["dog","racecar","car"]
+输出: ""
+解释: 输入不存在公共前缀。
+说明:
+	所有输入只包含小写字母 a-z 。
+*/
+
+func longestCommonPrefix(strs []string) string {
+	if len(strs) == 0 {
+		return ""
+	}
+
+	if len(strs) == 1 {
+		return strs[0]
+	}
+	length := len(strs[0])
+	max := -1
+loop:
+	for i := 0; i < length; i++ {
+		cur := strs[0][i]
+		for j := 1; j < len(strs); j++ {
+			if len(strs[j]) < i+1 || strs[j][i] != cur {
+				break loop
+			}
+		}
+		max = i
+	}
+	return strs[0][:max+1]
+}
---
+++ foundatimentals/structure/array_string/string/longest_palindrome.go
@@ -0,0 +1,39 @@
+package string
+
+/*
+最长回文子串
+给定一个字符串 s，找到 s 中最长的回文子串。你可以假设 s 的最大长度为 1000。
+示例 1：
+输入: "babad"
+输出: "bab"
+注意: "aba" 也是一个有效答案。
+示例 2：
+输入: "cbbd"
+输出: "bb"
+*/
+
+func longestPalindrome(s string) string {
+	if len(s) == 0 {
+		return ""
+	}
+	var start, end, res = 0, 1, 1
+	for i := 0; i < len(s); i++ {
+		for j := len(s) - 1; j > i; j-- {
+			if s[i] == s[j] {
+				var ok = true
+				for p1, p2 := i+1, j-1; p1 < p2; p1, p2 = p1+1, p2-1 {
+					if s[p1] != s[p2] {
+						ok = false
+						break
+					}
+				}
+
+				if ok && res < j-i+1 {
+					res = j - i + 1
+					start, end = i, j+1
+				}
+			}
+		}
+	}
+	return s[start:end]
+}
