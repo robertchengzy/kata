82776869570d5f307d466416aca26df6c4f6e70b
程智宇 <chengzy@ecqun.com>
2020-08-07 14:59:43 +0800

    study

--- main.go
+++ main.go
@@ -2,11 +2,8 @@ package main
 
 import (
 	"fmt"
-	"kata/foundatimentals/structure/array_string"
 )
 
 func main() {
 	fmt.Println("good good study, day day up")
-
-	fmt.Println(array_string.Generate(4))
 }
