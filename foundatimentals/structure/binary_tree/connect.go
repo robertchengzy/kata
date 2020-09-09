80643d19178c3f587120ae3471f5337e533c0a40
程智宇 <chengzy@ecqun.com>
2020-09-09 17:32:51 +0800

    study

--- foundatimentals/structure/binary_tree/connect.go
+++ foundatimentals/structure/binary_tree/connect.go
@@ -35,5 +35,49 @@ type Node struct {
 }
 
 func connect(root *Node) *Node {
+	if root == nil {
+		return nil
+	}
 
+	queue := []*Node{root}
+	for len(queue) > 0 {
+		size := len(queue)
+		for i := 0; i < size; i++ {
+			node := queue[0]
+			queue = queue[1:]
+			if i < size-1 {
+				node.Next = queue[0]
+			}
+
+			if node.Left != nil {
+				queue = append(queue, node.Left)
+			}
+
+			if node.Right != nil {
+				queue = append(queue, node.Right)
+			}
+		}
+	}
+
+	return root
+}
+
+func connect1(root *Node) *Node {
+	if root == nil {
+		return nil
+	}
+	leftMost := root
+	for leftMost.Left != nil {
+		head := leftMost
+		for head != nil {
+			head.Left.Next = head.Right
+			if head.Next != nil {
+				head.Right.Next = head.Next.Left
+			}
+
+			head = head.Next
+		}
+		leftMost = leftMost.Left
+	}
+	return root
 }
