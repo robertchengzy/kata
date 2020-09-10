5232256b72c2913397341033cf9005f05e71ae07
程智宇 <chengzy@ecqun.com>
2020-09-10 17:02:58 +0800

    study

--- foundatimentals/structure/binary_tree/serialize.go
+++ foundatimentals/structure/binary_tree/serialize.go
@@ -1,5 +1,10 @@
 package binary_tree
 
+import (
+	"strconv"
+	"strings"
+)
+
 /*
 二叉树的序列化与反序列化
 序列化是将一个数据结构或者对象转换为连续的比特位的操作，进而可以将转换后的数据存储在一个文件或者内存中，同时也可以通过网络传输到另一个计算机环境，采取相反方式重构得到原数据。
@@ -16,20 +21,52 @@ package binary_tree
 */
 
 type Codec struct {
+	l []string
 }
 
 func Constructor() Codec {
-
+	return Codec{}
 }
 
 // Serializes a tree to a single string.
 func (this *Codec) serialize(root *TreeNode) string {
-
+	return rserialize(root, "")
 }
 
 // Deserializes your encoded data to tree.
 func (this *Codec) deserialize(data string) *TreeNode {
+	l := strings.Split(data, ",")
+	for i := 0; i < len(l); i++ {
+		if l[i] != "" {
+			this.l = append(this.l, l[i])
+		}
+	}
+	return this.rdeserialize()
+}
 
+func rserialize(root *TreeNode, str string) string {
+	if root == nil {
+		str += "null,"
+	} else {
+		str += strconv.Itoa(root.Val) + ","
+		str = rserialize(root.Left, str)
+		str = rserialize(root.Right, str)
+	}
+	return str
+}
+
+func (this *Codec) rdeserialize() *TreeNode {
+	if this.l[0] == "null" {
+		this.l = this.l[1:]
+		return nil
+	}
+
+	val, _ := strconv.Atoi(this.l[0])
+	root := &TreeNode{Val: val}
+	this.l = this.l[1:]
+	root.Left = this.rdeserialize()
+	root.Right = this.rdeserialize()
+	return root
 }
 
 /**
@@ -38,3 +75,20 @@ func (this *Codec) deserialize(data string) *TreeNode {
  * data := obj.serialize(root);
  * ans := obj.deserialize(data);
  */
+
+/*
+思路和算法
+二叉树的序列化本质上是对其值进行编码，更重要的是对其结构进行编码。可以遍历树来完成上述任务。众所周知，我们一般有两个策略：BFS / DFS。
+	BFS 可以按照层次的顺序从上到下遍历所有的节点
+	DFS 可以从一个根开始，一直延伸到某个叶，然后回到根，到达另一个分支。根据根节点、左节点和右节点之间的相对顺序，可以进一步将DFS策略区分为：
+		先序遍历
+		中序遍历
+		后序遍历
+
+	我们也可以这样表示一颗二叉树：
+		如果当前的树为空，则表示为 X
+		如果当前的树不为空，则表示为 (<LEFT_SUB_TREE>)CUR_NUM(RIGHT_SUB_TREE)，其中：
+		<LEFT_SUB_TREE> 是左子树序列化之后的结果
+		<RIGHT_SUB_TREE> 是右子树序列化之后的结果
+		CUR_NUM 是当前节点的值
+*/
